e12e28239169b8f33f6ceca0fdc53c965d66eeba
Jackie Valeri <jackievaleri8@gmail.com>
2022-11-08 13:12:36 -0500

    add command line

--- main_classes/BioSeqAutoML_wrapper.py
+++ main_classes/BioSeqAutoML_wrapper.py
@@ -7,6 +7,7 @@
 import shutil
 from datetime import datetime
 from subprocess import call
+import argparse
 import warnings
 warnings.filterwarnings("ignore")
 import logging
@@ -600,4 +601,49 @@ def run_bioseqml(task, data_folder, data_file, sequence_type, model_folder, outp
         print('Task is not valid. Please choose one of binary_classification, multiclass_classification, or regression.')
 
     print("BioSeq-AutoML has concluded.")
+
+############## PART 5: FUNCTION FOR COMMAND LINE ##############
+
+if __name__ == '__main__':
+    print("#################################################################################################")
+    print("#######################               BIOSEQ-AUTOML            ##################")
+    print("#################################################################################################")
+    print('')
+    parser = argparse.ArgumentParser()
+
+    # required arguments
+    parser.add_argument('-task', '--task', required=True, help='str, one of \'binary_classification\', \'multiclass_classification\', \'regression\'')
+    parser.add_argument('-data_folder', '--data_folder', required=True, help='str representing folder where data is stored')
+    parser.add_argument('-data_file', '--data_file', required=True, help='str representing file name where data is stored')
+    parser.add_argument('-sequence_type', '--sequence_type', required=True, help='str, either \'nucleic_acid\', \'peptide\', or \'glycan\'')
+    parser.add_argument('-model_folder', '--model_folder', required=True, help='str representing folder where models are to be stored')
+    parser.add_argument('-output_folder', '--output_folder', required=True, help='str representing folder where models are to be stored')
+
+    # non-required arguments
+    parser.add_argument('-automl_search_techniques', '--automl_search_techniques', required=False, help='str representing which AutoML search technique should be performed, one of \'all\', \'deepswarm\', \'autokeras\', \'tpot\'')
+    parser.add_argument('-do_backup', '--do_backup', required=False, help='bool representing if a backup should be performed')
+    parser.add_argument('-max_runtime_minutes', '--max_runtime_minutes', required=False, help='int representing max runtime for model search in minutes')
+    parser.add_argument('-num_folds', '--num_folds', required=False, help='int representing num folds')
+    parser.add_argument('-verbosity', '--verbosity', required=False, help='int representing 0=not verbose, 1=verbose')
+    parser.add_argument('-do_auto_bin', '--do_auto_bin', required=False, help='bool representing if target values should be automatically binned')
+    parser.add_argument('-bin_threshold', '--bin_threshold', required=False, help='float representing threshold for positive and negative classes')
+    parser.add_argument('-do_transform', '--do_transform', required=False, help='bool representing if target values should be transformed')
+    parser.add_argument('-input_col', '--input_col', required=False, help='str representing input column name where sequences can be located')
+    parser.add_argument('-target_col', '--target_col', required=False, help='str representing target column name where target values can be located')
+    parser.add_argument('-pad_seqs', '--pad_seqs', required=False, help='str indicating pad_seqs method, either \'max\', \'min\', \'average\'')
+    parser.add_argument('-augment_data', '--augment_data', required=False, help='augment_data : str, either \'none\', \'complement\', \'reverse_complement\', or \'both_complements\'')
+    parser.add_argument('-dataset_robustness', '--dataset_robustness', required=False, help='bool indicating if data ablation study should be performed')
+    parser.add_argument('-num_final_epochs', '--num_final_epochs', required=False, help='int representing number of final epochs to train final deepswarm model')
+    parser.add_argument('-yaml_params', '--yaml_params', required=False, help='dict of extra deepswarm parameters, with keys \'max_depth\' (int), \'ant_count\' (int), \'epochs\' (int)')
+    parser.add_argument('-num_generations', '--num_generations', required=False, help='int representing number of generations of tpot search')
+    parser.add_argument('-population_size', '--population_size', required=False, help='int representing population size of tpot search')
+    parser.add_argument('-run_interpretation', '--run_interpretation', required=False, help='bool indicating if interpretation module should be executed')
+    parser.add_argument('-interpret_params', '--interpret_params', required=False, help='dict of extra interpretation parameters')
+    parser.add_argument('-run_design', '--run_design', required=False, help='bool indicating if design module should be executed')
+    parser.add_argument('-design_params', '--design_params', required=False, help='dict of extra design parameters')
+
+    args = parser.parse_args()
+
+    run_bioseqml(args.task, args.data_folder, args.data_file, args.sequence_type, args.model_folder, args.output_folder, automl_search_techniques = args.automl_search_techniques, do_backup = args.do_backup, max_runtime_minutes = args.max_runtime_minutes, num_folds = args.num_folds, verbosity = args.verbosity,  do_auto_bin = args.do_auto_bin, bin_threshold = args.bin_threshold, do_transform = args.do_transform, input_col = args.input_col, target_col = args.target_col, pad_seqs = args.pad_seqs, augment_data = args.augment_data, dataset_robustness = args.dataset_robustness, num_final_epochs = args.num_final_epochs, yaml_params = args.yaml_params, num_generations = args.num_generations, population_size = args.population_size, run_interpretation = args.run_interpretation, interpret_params = args.interpret_params, run_design = args.run_design, design_params = args.design_params)
+
 ############## END OF FILE ##############
